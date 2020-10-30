f02319459ba38e5394b6c131451ad1401df216a0
nova <nova.maene@gmail.com>
2020-10-30 14:31:50 +0100

    translatez

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ container.addEventListener('mousemove', (e) => {
   const xAxis = (window.innerWidth / 2 - e.pageX) / 25;
   const yAxis = (window.innerHeight / 2 - e.pageY) / 25;
   // console.log(xAxis, yAxis)
-  card.style.transform = `rotateY(${-xAxis}deg) rotateX(${yAxis}deg) translateZ(1px)`;
+  card.style.transform = `rotateY(${-xAxis}deg) rotateX(${yAxis}deg)`;
 
 });
 
